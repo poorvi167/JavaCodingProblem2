386b5606cd5cc9ffd6ba62c68c8e63ec5ef20954
dbc2201 <divyanshb22@gmail.com>
2022-05-15 01:52:34 +0530

    add the Test class

---
+++ src/test/java/io/github/dbc/NonRepeatedCharactersFinderTest.java
@@ -0,0 +1,20 @@
+package io.github.dbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NonRepeatedCharactersFinderTest {
+
+    private NonRepeatedCharactersFinder finder;
+
+    @BeforeEach
+    void setUp() {
+        finder = new NonRepeatedCharactersFinder();
+    }
+
+    @Test
+    void firstNonRepeatedCharacter() {
+        Assertions.fail();
+    }
+}
