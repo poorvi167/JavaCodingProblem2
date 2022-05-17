5e1e61c363991b5d6577bb0aa5a76dc396225890
dbc2201 <divyanshb22@gmail.com>
2022-05-18 00:20:39 +0530

    add the Main class

--- src/main/java/io/github/dbc/Main.java
+++ src/main/java/io/github/dbc/Main.java
@@ -2,5 +2,8 @@ package io.github.dbc;
 
 public class Main {
     public static void main(String[] args) {
+        NonRepeatedCharactersFinder finder = new NonRepeatedCharactersFinder();
+        char firstNonRepeatedCharacter = finder.findFirstNonRepeatedCharacter("aabbcc");
+        System.out.println(firstNonRepeatedCharacter);
     }
 }
